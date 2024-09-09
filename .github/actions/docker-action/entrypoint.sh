121f3f2dbcfb999a4475723a63861a5a070efdc7
Mariana Alcantara <marigigialcantara40@gmail.com>
2024-09-09 15:01:10 -0300

    update entrypoint

--- .github/actions/docker-action/entrypoint.sh
+++ .github/actions/docker-action/entrypoint.sh
@@ -3,3 +3,7 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
+
+# pra dar permissão no arquivo, pelo git executar no diretório
+# git update-index --chmod=+x entrypoint.sh
+# git ls-files --stage entrypoint.sh
